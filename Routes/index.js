388c8a881d604f6bf49f9ebfcd21d340ad8a122b
JunxiC <chenjon09@gmail.com>
2024-03-07 16:34:54 -0500

    commit

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/songController");
+const authController = require("../Controllers/authController");
 
 router.get("/", shopController.getSongs);
 router.post("/add", shopController.createSong);
