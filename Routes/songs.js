7f946d847810aa3a97e65dc969ef1283dcf4db83
timothyd93 <timothydeng93@gmail.com>
2024-03-21 15:43:18 -0400

    a

--- Routes/songs.js
+++ Routes/songs.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const router = new express.Router();
 const songController = require("../Controllers/songController");
-const authController = require("../Controllers/authController");
 
 router.get("/", songController.getSongs);
 router.post("/add", songController.createSong);
 router.patch("/shop/:id", songController.updateSongs);
 router.delete("/shop/:id", songController.deleteSong);
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+
 
 module.exports = router;
 
