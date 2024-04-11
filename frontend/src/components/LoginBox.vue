6b6746a7b20eda753472ee83ba0d3a33d7405218
JunxiC <chenjon09@gmail.com>
2024-04-11 16:05:02 -0400

    styles

--- frontend/src/components/LoginBox.vue
+++ frontend/src/components/LoginBox.vue
@@ -57,41 +57,62 @@ function displayUser() {
 </script>
 
 <style scoped>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    text-align: center;
-  }
-}
+@import url(https://fonts.googleapis.com/css?family=Roboto:300);
 
+.about {
+  width: 360px;
+  padding: 8% 0 0;
+  margin: auto;
+}
 .logincard {
-  background-color: green;
-  padding: 20px;
-  border-radius: 10px;
-}
-
-.login {
-}
-
-.form-group {
   position: relative;
-  min-height: 3.5em;
+  z-index: 1;
+  background: #FFFFFF;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
 }
-
-input.form-control {
-  height: 3em;
-  position: absolute;
-  top: 0;
-  left: 0;
+.logincard input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #f2f2f2;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
 }
-
-label.form-control-label {
-  position: absolute;
-  font-size: .8em;
-  top: 0;
-  left: 5px;
+.logincard button {
+  font-family: "Roboto", sans-serif;
   text-transform: uppercase;
+  outline: 0;
+  background: #4CAF50;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
 }
+.logincard button:hover,.logincard button:active,.logincard button:focus {
+  background: #43A047;
+}
+.logincard .message {
+  margin: 15px 0 0;
+  color: #b3b3b3;
+  font-size: 12px;
+}
+.logincard .message a {
+  color: #4CAF50;
+  text-decoration: none;
+}
+.logincard .register-form {
+  display: none;
+}
+
 </style>
