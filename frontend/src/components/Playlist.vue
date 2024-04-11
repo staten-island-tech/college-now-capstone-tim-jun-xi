866d8c8c78142467d6e448acb3c5de981a0667b4
timothyd93 <timothydeng93@gmail.com>
2024-04-11 15:47:23 -0400

    a

---
+++ frontend/src/components/Playlist.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+  
+  <h1 v-for="song in songs">{{ song.name }}</h1>
+  
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted, ref } from "vue";
+  const songs = ref("");
+  async function getShops() {
+    let res = await fetch("http://localhost:3001");
+  
+    let data = await res.json();
+    songs.value = data;
+    console.log(data);
+  }
+  
+  onMounted(() => {
+    getShops();
+  });
+  </script>
+  
+  <style scoped></style>
