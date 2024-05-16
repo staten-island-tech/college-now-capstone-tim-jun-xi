c03c8d739f2f632887b69f61c595990e5e6810bf
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2024-05-15 21:42:47 -0400

    a

--- frontend/src/stores/authStore.js
+++ frontend/src/stores/authStore.js
@@ -3,7 +3,8 @@ import { defineStore } from 'pinia'
 export const useAuthStore = defineStore({
   id: 'auth',
   state: () => ({
-    isAuthenticated: false
+    isAuthenticated: false,
+    isRegistered: false
   }),
   actions: {
     async login({ username, password }) {
@@ -23,7 +24,31 @@ export const useAuthStore = defineStore({
         if (res.ok) {
           this.isAuthenticated = true
         } else {
-          console.log('no works')
+          console.log('Login failed')
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async register({ username, password }) {
+      try {
+        const res = await fetch('http://localhost:3001/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: username.toLowerCase(),
+            password: password
+          })
+        })
+        const data = await res.json()
+        console.log(data)
+        if (res.ok && data.success) {
+          this.isRegistered = true
+        } else {
+          console.log('Registration failed')
+          alert('Enter a unique username')
         }
       } catch (error) {
         console.log(error)
